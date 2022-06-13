23e7270003c1b1ec3f51bce65d673e3bddaf38a3
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-06-13 14:57:48 +0700

    Create SwitchOld.java

---
+++ Java/Sesi 1/SwitchOld.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+
+class SwitchOld {
+  public static void main(String[] args) {
+    String day, result = "";
+    Scanner input = new Scanner(System.in);
+    System.out.print("Input Char: ");
+    day = input.nextLine();
+    switch (day){
+        case "M":
+        case "W":
+        case "F": {
+            result = "MWF";
+            break;
+        }
+        case "T":
+        case "TH":
+        case "S": {
+            result = "TTS";
+            break;
+        }
+    };
+    System.out.format("Old switch results: %s", result);
+  }
+}
