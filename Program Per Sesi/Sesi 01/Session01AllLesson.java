eb4192feac62cb9a5b04f10414a565996016a7a8
jampirojam <jampirojam@gmail.com>
2022-06-14 21:52:45 +0700

    fix format double

--- Program Per Sesi/Sesi 01/Session01AllLesson.java
+++ Program Per Sesi/Sesi 01/Session01AllLesson.java
@@ -29,7 +29,7 @@ class Session01AllLesson {
                 tot += umurkan.get(i);
                 rata = tot / umurkan.size();
             }            
-            System.out.print(tot+" "+rata);
+            System.out.format("Total umur kandidat adalah %,.2f tahun, sedang rata-rata umurnya adalah %,.2f tahun", tot, rata);
         } catch (IOException mantan) {
             System.out.println(mantan);
         }
