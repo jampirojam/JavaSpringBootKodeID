487576a9b290348c80fcc1893ab4dad1f00e8ef4
jampirojam <jampirojam@gmail.com>
2022-06-16 22:38:54 +0700

    tambah data

---
+++ Program Per Sesi/Sesi 05/Driver/Main.java
@@ -0,0 +1,9 @@
+package Driver;
+
+public class Main {
+    public static void main(String[] args) {
+        Anak anak = new Anak();
+        Aktivitas akt = new Aktivitas();
+        OrangDewasa od = new OrangDewasa();
+    }
+}
