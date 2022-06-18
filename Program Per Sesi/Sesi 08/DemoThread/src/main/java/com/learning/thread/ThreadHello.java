d4cb1d135aa91179b04d7e2a441fe839bdf9c808
jampirojam <jampirojam@gmail.com>
2022-06-18 11:57:30 +0700

    Fix jUnit

---
+++ Program Per Sesi/Sesi 08/DemoThread/src/main/java/com/learning/thread/ThreadHello.java
@@ -0,0 +1,22 @@
+package com.learning.thread;
+
+import java.util.logging.*;
+import java.util.*;
+
+public class ThreadHello implements Runnable {
+
+    private final String name;
+
+    public ThreadHello(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        System.out.println(name + " : Hello");
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadHello.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
